563e9e9a814bb6a0fcbfb0bf199f3019f8bacc4f
carolinacervelaf <carolinacervelaf@gmail.com>
2020-10-13 19:02:19 -0300

    product part

--- src/routeComponent/profile/Profile.js
+++ src/routeComponent/profile/Profile.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import photoProfile from '../../../src/photo-card.jpg'
-//import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 //import ConfirmationModal from '../../components/ConfirmationModal';
 
 function Profile() {
+
+
+
 return (
     <div className='div-mother-profile '>
         <div className='row'>
         <div className='col-4'>
                 <h3>Profile</h3>
                 <hr/>
+                <Link to="/profile-edit"><i class="far fa-edit"></i></Link>
+            {/*     <Link to={{pathname: `/profile/delete/${user._id}`}}><i className="far fa-trash-alt"></i></ Link> */}
                 <img className='photo-profile' src={photoProfile}/>
             </div>
             <div className='col-2 card-profile'>
@@ -43,7 +48,6 @@ return (
              card-profile'>
                 coooool
             </div>
-            
         </div>
     </div>
 )
@@ -59,10 +63,21 @@ export default Profile;
         /> */
 
 
-//icon instagram - <i className="fab fa-instagram"></i>
+//icon instagram - <a to=""><i className="fab fa-instagram"></i></a>
 
-//icon twitter - 
+//icon twitter - <a></a><i className="fab fa-twitter-square"></i></a>
 
-//icon facebook - 
+//icon facebook - <a></a><i className="fab fa-facebook-square"></i></a>
 
-//icon youtube - 
+//icon youtube - <a><i className="fab fa-youtube-square"></i></a>
+
+//delete profile - <Link to={{pathname: `/profile/delete/${user._id}`}}><i className="far fa-trash-alt"></i></ Link>
+
+
+/* const handleModalToggle = (userId) => {
+    setState((prevState) => ({
+        showModal: !prevState.showModal,
+        selectedRowId: userId,
+    }));
+}; */
+
