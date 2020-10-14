dd44a1b9e2a0d869499ae8be513d82e3409256b1
carolinacervelaf <carolinacervelaf@gmail.com>
2020-10-14 19:51:47 -0300

    done product edit, create, delete

--- src/routeComponent/profile/Profile.js
+++ src/routeComponent/profile/Profile.js
@@ -48,14 +48,14 @@ return (
             <div className='col-2 card-profile'>
                 coooool
             </div>
-            {product.map((products) => <div class="card" style="width: 18rem;">
+{/*             {product.map((products) => <div class="card" style="width: 18rem;">
   <img src="..." class="card-img-top" alt="..." />
   <div class="card-body">
     <h5 class="card-title">{product.title}</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
-</div>)}
+</div>)} */}
 
         </div>
         <div className='row'>
