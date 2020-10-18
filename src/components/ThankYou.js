22b86a6a824dab542006cf51df7aaf2bba31742b
yordancurty <yordancurty@gmail.com>
2020-10-17 23:22:38 -0300

    Cart e productDetails funcionando... pagina thanYoumelhorada um pouco

--- src/components/ThankYou.js
+++ src/components/ThankYou.js
@@ -8,7 +8,7 @@ function ThankYou(){
 return (
         <div>
             <h1>
-                <strong>
+                <strong className ="thakYouMsg">
                 Obrigado pela compra! Graças a você mais um artista 
                 independente porá continuar a divulgar sua arte
                 </strong>
